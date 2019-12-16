9f72915bd9505c04c4ce18fc49f3c15587aa76e8
MelvinMcMonagle <36984767+MelvinMcMonagle@users.noreply.github.com>
2019-12-16 18:33:34 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app5">
+    <MainContent/>
+  </div>
+</template>
+
+<script>
+import MainContent from './components/MainContent.vue'
+
+export default {
+  name: 'app5',
+  components: {
+    MainContent
+  }
+}
+</script>
+
+<style>
+#app5 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="mainContainer">
+      <h2>Test</h2>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+
+
+}
+</script>
+
+<style scoped>
+
+.mainContainer{
+  height: 400px;
+  background: white;
+  width:500px;
+}
+
+
+
+</style>
